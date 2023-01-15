bac076af55e818a0b8bfa334437d7c168e344d4a
MatiasS.dev() <94636694+MatiasSilvestri@users.noreply.github.com>
2023-01-15 14:17:03 -0300

    Add files via upload

---
+++ criptomonedasPrecio/main.py
@@ -0,0 +1,27 @@
+from lib import ccxt, yfinance
+import pandas_at as ta
+import pandas as pd
+from lib import ccxt
+
+exchange=ccxt.binance()
+bars=exchange.fetch_obicv('ETH/USDT', timefeame='5m', limit=500)
+df=pd.DataFrame(bars, columns=['time', 'open', 'high', 'low', 'close', 'volume'])
+print(df)
+
+adx=df.ta.adx()
+macd=df.ta.macd(fast=14, slow=28)
+print(macd)
+rsi=df.ta.rsi()
+
+print(macd)
+add= ta.adx(df['high'], df['low'], df['close'])
+adx=df.ta.adx()
+df=pd.concat([df, adx, macd, rsi], axis=1)
+print(df)
+
+rsi=df.ta.rsi()
+df=pd.contact([df, adx, macd, rsi], axis=1)
+print(df)
+df=df[df['REST_14']<30]
+
+print(adx.tail(20))
